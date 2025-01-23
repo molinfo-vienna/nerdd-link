722a9d787ad3f6ca247196fb56ec4ac3b12ff8c5
Steffen Hirte <shirte@users.noreply.github.com>
2025-01-24 00:19:03 +1300

    fix: Reroute exceptions from threads to main thread

--- nerdd_link/delegates/split_and_merge_step.py
+++ nerdd_link/delegates/split_and_merge_step.py
@@ -1,4 +1,5 @@
 from functools import reduce
+from queue import Queue
 from threading import Thread
 from typing import Any, Iterator, List
 
@@ -26,18 +27,26 @@ class SplitAndMergeStep(OutputStep):
         # Note: we use a thread-safe variant of tee.
         source_copies = safetee(source, len(self._step_lists))
 
+        # When a thread throws an exception, it won't be caught by the main thread. That is why
+        # we create an exception bucket and add the exceptions to it. The main thread will then
+        # raise the first exception in the bucket.
+        exception_bucket: Queue = Queue()
+
         def _run_steps(steps: List[Step], source: Iterator[dict]) -> Any:
-            assert len(steps) > 0, "There must be at least one step."
+            try:
+                assert len(steps) > 0, "There must be at least one step."
 
-            output_step = steps[-1]
-            assert isinstance(output_step, OutputStep), "The last step must be an OutputStep."
+                output_step = steps[-1]
+                assert isinstance(output_step, OutputStep), "The last step must be an OutputStep."
 
-            # Concatenate the steps in each step list.
-            # e.g. step_list = [step1, step2, step3]
-            # --> step3(step2(step1(source)))
-            reduce(lambda x, y: y(x), steps, source)
+                # Concatenate the steps in each step list.
+                # e.g. step_list = [step1, step2, step3]
+                # --> step3(step2(step1(source)))
+                reduce(lambda x, y: y(x), steps, source)
 
-            return output_step.get_result()
+                return output_step.get_result()
+            except Exception as e:
+                exception_bucket.put(e)
 
         threads = [
             Thread(target=_run_steps, args=(step_list, source_copy))
@@ -49,3 +58,7 @@ class SplitAndMergeStep(OutputStep):
 
         for thread in threads:
             thread.join()
+
+        # raise errors that occurred in the threads
+        if not exception_bucket.empty():
+            raise exception_bucket.get()
