67ae451c827a3616fc518bba97a065bbe7739655
Steffen Hirte <shirte@users.noreply.github.com>
2025-04-30 17:30:22 +1200

    fix: Let SerializeJobAction handle tombstones

--- nerdd_link/actions/serialize_job_action.py
+++ nerdd_link/actions/serialize_job_action.py
@@ -1,12 +1,13 @@
 import json
 import logging
+import os
 
 from nerdd_module import OutputStep
 
 from ..channels import Channel
 from ..delegates import ReadPickleStep, SerializeJobModel
 from ..files import FileSystem
-from ..types import SerializationRequestMessage, SerializationResultMessage
+from ..types import SerializationRequestMessage, SerializationResultMessage, Tombstone
 from .action import Action
 
 __all__ = ["SerializeJobAction"]
@@ -27,6 +28,12 @@ class SerializeJobAction(Action[SerializationRequestMessage]):
         output_format = message.output_format
         logger.info(f"Write output for job {job_id} in format {output_format}")
 
+        # check input files
+        input_files = list(self._file_system.iter_results_file_paths(job_id))
+        if len(input_files) == 0:
+            logger.warning(f"No input files found for job {job_id}. Cannot serialize.")
+            return
+
         # remove specific parameter keys that could induce vulnerabilities
         params.pop("output_file", None)
         params.pop("output_format", None)
@@ -43,7 +50,7 @@ class SerializeJobAction(Action[SerializationRequestMessage]):
 
         steps = [
             # read the result checkpoint files in the correct order
-            ReadPickleStep(self._file_system.iter_results_file_handles(job_id)),
+            ReadPickleStep(self._file_system.iter_results_file_handles(job_id, mode="rb")),
             # don't preprocess, don't do prediction, only post-process
             *model._get_postprocessing_steps(output_format, output_file=output_file, **params),
         ]
@@ -61,4 +68,18 @@ class SerializeJobAction(Action[SerializationRequestMessage]):
 
         await self.channel.serialization_results_topic().send(
             SerializationResultMessage(job_id=job_id, output_format=output_format)
+        )
+
+    async def _process_tombstone(self, message: Tombstone[SerializationRequestMessage]) -> None:
+        job_id = message.job_id
+        output_format = message.output_format
+        logger.info(f"Received tombstone for job {job_id} in format {output_format}")
+
+        # remove the output file if it exists
+        output_file = self._file_system.get_output_file(job_id, output_format)
+        if os.path.exists(output_file):
+            os.remove(output_file)
+
+        await self.channel.serialization_results_topic().send(
+            Tombstone(SerializationResultMessage, job_id=job_id, output_format=output_format)
         )
