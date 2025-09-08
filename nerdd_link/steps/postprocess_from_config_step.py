d3fdcb357fb851dd76f1a9cabd67a2a87f74f462
Steffen Hirte <shirte@users.noreply.github.com>
2025-09-08 21:05:59 +1200

    fix: Extract code to PostprocessFromConfigStep

---
+++ nerdd_link/steps/postprocess_from_config_step.py
@@ -0,0 +1,65 @@
+from typing import IO, Any, Iterator, List, Optional
+
+from nerdd_module import Model, OutputStep, Step
+from nerdd_module.config import Configuration, DictConfiguration
+from rdkit.Chem import Mol
+
+from ..types import SerializationResultMessage
+
+__all__ = ["PostprocessFromConfigStep"]
+
+
+class DummyModel(Model):
+    def __init__(self, config: dict) -> None:
+        super().__init__()
+        self._config = config
+
+    def _get_config(self) -> Configuration:
+        return DictConfiguration(self._config)
+
+    def _predict_mols(self, mols: List[Mol], **kwargs: Any) -> List[dict]:
+        # We will only extract the postprocessing steps of this model and the predict method
+        # will never be called.
+        return []
+
+
+class PostprocessFromConfigStep(Step):
+    def __init__(
+        self,
+        config: dict,
+        job_id: str,
+        output_format: str,
+        output_file: Optional[IO] = None,
+        **params: Any,
+    ) -> None:
+        super().__init__()
+        self._config = config
+        self._job_id = job_id
+        self._output_format = output_format
+        self._output_file = output_file
+        self._params = params
+
+    def _run(self, source: Optional[Iterator[dict]] = None) -> Iterator[dict]:
+        # extract postprocessing steps specified through the configuration
+        model = DummyModel(self._config)
+        postprocessing_steps = model._get_postprocessing_steps(
+            self._output_format, output_file=self._output_file, **self._params
+        )
+
+        # build the pipeline from the list of steps
+        pipeline = source
+        for t in postprocessing_steps:
+            pipeline = t(pipeline)
+
+        output_step = postprocessing_steps[-1]
+        assert isinstance(output_step, OutputStep), "The last step must be an OutputStep."
+
+        output_step.get_result()
+
+        # send a message that the serialization is done
+        yield {
+            "topic": "serialization-results",
+            "message": SerializationResultMessage(
+                job_id=self._job_id, output_format=self._output_format
+            ),
+        }
