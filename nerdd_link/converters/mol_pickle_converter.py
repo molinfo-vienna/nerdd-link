f834c8d722e1aa523a31e7b1dcb410cd25f5513e
Steffen Hirte <shirte@users.noreply.github.com>
2025-09-10 21:13:11 +1200

    fix: Always convert to PropertyMol when using pickle

--- nerdd_link/converters/mol_pickle_converter.py
+++ nerdd_link/converters/mol_pickle_converter.py
@@ -9,6 +9,7 @@ __all__ = ["MolPickleConverter"]
 
 class MolPickleConverter(Converter):
     def _convert(self, input: Any, context: dict) -> Any:
+        # use PropertyMol in order to keep molecular properties (thanks, RDKit! :/ )
         if isinstance(input, Mol):
             return PropertyMol(input)
         else:
