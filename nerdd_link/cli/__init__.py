3b96a2253ff41c4414a75f0dcb9e76b817458a54
Steffen Hirte <shirte@users.noreply.github.com>
2025-10-02 14:37:00 +1300

    fix: Remove command for initializing the system

--- nerdd_link/cli/__init__.py
+++ nerdd_link/cli/__init__.py
@@ -1,4 +1,5 @@
-from .initialize_system import *
-from .run_job_server import *
-from .run_prediction_server import *
-from .run_serialization_server import *
+from .run_job_server import run_job_server
+from .run_prediction_server import run_prediction_server
+from .run_serialization_server import run_serialization_server
+
+__all__ = ["run_job_server", "run_prediction_server", "run_serialization_server"]
