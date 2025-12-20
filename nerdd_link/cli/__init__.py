2c8ccdad5145707387eeadf27bd9852d662a5110
Steffen Hirte <shirte@users.noreply.github.com>
2025-12-20 17:05:21 +1300

    fix: Add CLI entrypoint for initialize_system

--- nerdd_link/cli/__init__.py
+++ nerdd_link/cli/__init__.py
@@ -1,5 +1,11 @@
+from .initialize_system import initialize_system
 from .run_job_server import run_job_server
 from .run_prediction_server import run_prediction_server
 from .run_serialization_server import run_serialization_server
 
-__all__ = ["run_job_server", "run_prediction_server", "run_serialization_server"]
+__all__ = [
+    "initialize_system",
+    "run_job_server",
+    "run_prediction_server",
+    "run_serialization_server",
+]
