8f298eb263911dc4d453f59b64ee774e253ea248
Steffen Hirte <shirte@users.noreply.github.com>
2025-12-29 13:43:28 +1300

    test: Check that PickleWriter does not raise exceptions when writing non-picklable objects

---
+++ tests/basic/test_pickle_writer.py
@@ -0,0 +1,12 @@
+import tempfile
+
+from nerdd_link import PickleWriter
+
+
+def test_unpicklable_values():
+    with tempfile.NamedTemporaryFile() as temp_file:
+        pickle_writer = PickleWriter(temp_file)
+
+        # temp_file is a _io.BufferedRandom object that cannot be pickled
+        # pickle_writer should not raise an error
+        pickle_writer.write([{ "file": temp_file }])
