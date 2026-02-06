ccfcf6e8ea0f420b1598a6f9824f5e78da4eb185
Steffen Hirte <shirte@users.noreply.github.com>
2026-02-06 17:03:45 +1300

    test: Implement DummyStorage to test path roundtrips

--- tests/basic/test_storage.py
+++ tests/basic/test_storage.py
@@ -1,32 +1,52 @@
 from nerdd_link import (
     CheckpointFilePathSpec,
-    FileSystemStorage,
     ModuleFilePathSpec,
     OutputFilePathSpec,
     PropertyFilePathSpec,
     ResultCheckpointFilePathSpec,
     SourceFilePathSpec,
+    Storage,
 )
 
 
-def test_module_file_path_round_trip(tmp_path):
-    storage = FileSystemStorage(str(tmp_path))
+class DummyStorage(Storage):
+    def __init__(self) -> None:
+        super().__init__("dummy")
+
+    def _validate(self) -> None:
+        raise NotImplementedError
+
+    def _iter_directory(self, identifier):
+        raise NotImplementedError
+
+    def _file_exists(self, identifier):
+        raise NotImplementedError
+
+    def _get_binary_file_handle(self, identifier, mode):
+        raise NotImplementedError
+
+    def _delete_file(self, identifier):
+        raise NotImplementedError
+
+
+def test_module_file_path_round_trip():
+    storage = DummyStorage()
 
     file_path = storage.get_module_file_path("module-1")
 
     assert storage.parse_module_file_path(file_path) == ModuleFilePathSpec(module_id="module-1")
 
 
-def test_source_file_path_round_trip(tmp_path):
-    storage = FileSystemStorage(str(tmp_path))
+def test_source_file_path_round_trip():
+    storage = DummyStorage()
 
     file_path = storage.get_source_file_path("source-1")
 
     assert storage.parse_source_file_path(file_path) == SourceFilePathSpec(source_id="source-1")
 
 
-def test_checkpoint_file_path_round_trip(tmp_path):
-    storage = FileSystemStorage(str(tmp_path))
+def test_checkpoint_file_path_round_trip():
+    storage = DummyStorage()
 
     file_path = storage.get_checkpoint_file_path("job-1", 2)
 
@@ -35,8 +55,8 @@ def test_checkpoint_file_path_round_trip(tmp_path):
     )
 
 
-def test_result_checkpoint_file_path_round_trip(tmp_path):
-    storage = FileSystemStorage(str(tmp_path))
+def test_result_checkpoint_file_path_round_trip():
+    storage = DummyStorage()
 
     file_path = storage.get_result_checkpoint_file_path("job-1", 2)
 
@@ -45,8 +65,8 @@ def test_result_checkpoint_file_path_round_trip(tmp_path):
     )
 
 
-def test_property_file_path_round_trip(tmp_path):
-    storage = FileSystemStorage(str(tmp_path))
+def test_property_file_path_round_trip():
+    storage = DummyStorage()
 
     file_path = storage.get_property_file_path("job-1", "logp", "record-1")
 
@@ -55,8 +75,8 @@ def test_property_file_path_round_trip(tmp_path):
     )
 
 
-def test_output_file_path_round_trip(tmp_path):
-    storage = FileSystemStorage(str(tmp_path))
+def test_output_file_path_round_trip():
+    storage = DummyStorage()
 
     file_path = storage.get_output_file_path("job-1", "sdf.gz")
 
