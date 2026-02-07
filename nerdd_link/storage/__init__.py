d6fa3ac0f4048f6899c42f3c64369dc3f273e6c9
Steffen Hirte <shirte@users.noreply.github.com>
2026-02-07 18:59:10 +1300

    fix: Use explicit imports in storage module

--- nerdd_link/storage/__init__.py
+++ nerdd_link/storage/__init__.py
@@ -1,5 +1,29 @@
-from .chained_storage import *
-from .file_system_storage import *
-from .mirrored_storage import *
-from .storage import *
-from .wrong_prefix_error import *
+from .chained_storage import ChainedStorage
+from .file_system_storage import FileSystemStorage
+from .mirrored_storage import MirroredStorage
+from .s3_storage import S3Storage
+from .storage import (
+    CheckpointFilePathSpec,
+    ModuleFilePathSpec,
+    OutputFilePathSpec,
+    PropertyFilePathSpec,
+    ResultCheckpointFilePathSpec,
+    SourceFilePathSpec,
+    Storage,
+)
+from .wrong_prefix_error import WrongPrefixError
+
+__all__ = [
+    "ChainedStorage",
+    "CheckpointFilePathSpec",
+    "FileSystemStorage",
+    "MirroredStorage",
+    "ModuleFilePathSpec",
+    "OutputFilePathSpec",
+    "PropertyFilePathSpec",
+    "ResultCheckpointFilePathSpec",
+    "S3Storage",
+    "SourceFilePathSpec",
+    "Storage",
+    "WrongPrefixError",
+]
